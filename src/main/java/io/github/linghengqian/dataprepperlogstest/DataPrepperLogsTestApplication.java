1c7b60aa37046d77263b33df2bb2a38326041e70
linghengqian <linghengqian@outlook.com>
2024-10-26 16:49:10 +0800

    Init

---
+++ src/main/java/io/github/linghengqian/dataprepperlogstest/DataPrepperLogsTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.linghengqian.dataprepperlogstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPrepperLogsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataPrepperLogsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/linghengqian/dataprepperlogstest/DataPrepperLogsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.linghengqian.dataprepperlogstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPrepperLogsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
